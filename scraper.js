3741a58ab9658628680ac6c69c07aebcf753231c
axjs <a-x@ukr.net>
2016-07-06 16:26:53 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -5,6 +5,18 @@ var request = require("request");
 var tress = require('tress');
 var sqlite3 = require("sqlite3").verbose();
 
+var q = tress(function(job, done){
+    console.log('hello ' + job.name);
+    someAsyncFunction(job, function(err, data){
+        if (err) {
+            done(err);
+        } else {
+            done(null, data);
+        }
+    });
+}, 2);
+
+
 function initDatabase(callback) {
 	// Set up sqlite database.
 	var db = new sqlite3.Database("data.sqlite");
@@ -41,34 +53,35 @@ function fetchPage(url, callback) {
 	});
 }
 
+function processPage (body) {
+	// Use cheerio to find things in the page with css selectors.
+	var $ = cheerio.load(body);
+
+	//var elements = $("article.product-item h3.product-item-headline a").each(function () {
+	//	var value = $(this).text().trim();
+	//	updateRow(db, value);
+	//});
+	var cat = $('nav.breadcrumbs ul').text().replace(/(\n|\t)+/g, '/').trim()
+	var data = $("article.product-item").map(function(indx, el){
+      		var $el = $(el)
+      		var res = {
+      			cat: cat,
+        		name: $el.find('a').text(), 
+        		oldPrice: $el.find('small').text(), 
+        		price: $el.find('.product-item-price').text().trim().split('\n')[0], 
+        		image: $el.find('img').attr('data-image')
+      		} 
+      		updateRow(db, JSON.stringify(res, 0 ,2));
+      		return res
+	})
+	
+	readRows(db);
+	db.close();
+}
+	
 function run(db) {
 	// Use request to read in pages.
-	fetchPage("http://www2.hm.com/en_gb/sale/kids/viewall.html?product-type=kids_all&sort=stock&offset=0&page-size=20", function (body) {
-		// Use cheerio to find things in the page with css selectors.
-		var $ = cheerio.load(body);
-
-		//var elements = $("article.product-item h3.product-item-headline a").each(function () {
-		//	var value = $(this).text().trim();
-		//	updateRow(db, value);
-		//});
-		var cat = $('nav.breadcrumbs ul').text().replace(/(\n|\t)+/g, '/').trim()
-		var data = $("article.product-item").map(function(indx, el){
-      			var $el = $(el)
-      			var res = {
-      				cat: cat,
-        			name: $el.find('a').text(), 
-        			oldPrice: $el.find('small').text(), 
-        			price: $el.find('.product-item-price').text().trim().split('\n')[0], 
-        			image: $el.find('img').attr('data-image')
-      			} 
-      			updateRow(db, JSON.stringify(res, 0 ,2));
-      			return res
-		})
-		
-		readRows(db);
-
-		db.close();
-	});
+	fetchPage("http://www2.hm.com/en_gb/sale/kids/viewall.html?product-type=kids_all&sort=stock&offset=0&page-size=20", processPage);
 }
 
 initDatabase(run);
